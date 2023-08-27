dbf254d1de3d9058c77380f964424f845f6b1f3f
vap1 <vaibhavvasant1705@gmail.com>
2023-08-27 17:26:34 +0530

    First Commit

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import AppNavigator from './src/navigation/AppNavigator';
+import { AppNavigator } from './navigation/AppNavigator';
 
 const Stack = createStackNavigator();
 
