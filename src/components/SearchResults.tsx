e6df4e2b2b5738d88228ab932d983f67c84658e4
vap1 <vaibhavvasant1705@gmail.com>
2023-08-27 17:21:33 +0530

    First Commit

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,23 +1,29 @@
 
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 import { User } from '../types/UserTypes';
+import { SearchResponse } from '../apis/SearchApi';
 
-interface SearchResultsProps {
+type SearchResultsProps = {
   searchResults: User[];
-}
+};
 
 const SearchResults: React.FC<SearchResultsProps> = ({ searchResults }) => {
   return (
     <View>
-      {searchResults.map((user) => (
-        <View key={user.userId}>
-          <Text>{user.firstName} {user.lastName}</Text>
-          <Text>Email: {user.email}</Text>
-          <Text>Phone: {user.phone}</Text>
-          <Text>Address: {user.address}</Text>
-        </View>
-      ))}
+      <Text>Search Results:</Text>
+      <FlatList
+        data={searchResults}
+        keyExtractor={(item) => item.userId}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.firstName} {item.lastName}</Text>
+            <Text>Email: {item.email}</Text>
+            <Text>Phone: {item.phone}</Text>
+            <Text>Address: {item.address}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
