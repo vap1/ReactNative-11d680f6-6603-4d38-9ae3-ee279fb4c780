088d05ac1b54a21269220a4cce212ef8462220d6
vap1 <vaibhavvasant1705@gmail.com>
2023-08-27 17:12:29 +0530

    First Commit

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import { AppNavigator } from './navigation/AppNavigator';
 
 const Stack = createStackNavigator();
 
-const App: React.FC = () => {
+const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
