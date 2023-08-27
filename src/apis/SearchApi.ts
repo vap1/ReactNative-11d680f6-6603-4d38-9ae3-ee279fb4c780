1f842519c461f5fc4d5d29684f52397316eb294c
vap1 <vaibhavvasant1705@gmail.com>
2023-08-27 17:21:35 +0530

    First Commit

--- src/apis/SearchApi.ts
+++ src/apis/SearchApi.ts
@@ -1,7 +1,6 @@
 
 import axios from 'axios';
 import { SearchRequest, SearchResponse } from '../types/SearchTypes';
-import { User } from '../types/UserTypes';
 
 const BASE_URL = 'http://your-api-base-url.com'; // Replace with your actual API base URL
 
@@ -10,6 +9,6 @@ export const search = async (keyword: string): Promise<SearchResponse> => {
     const response = await axios.get<SearchResponse>(`${BASE_URL}/search/${keyword}`);
     return response.data;
   } catch (error) {
-    throw new Error('Failed to search. Please try again.'); // Handle error as per your app's requirements
+    throw new Error('Failed to search. Please try again.'); // Handle error based on your app's requirements
   }
 };
