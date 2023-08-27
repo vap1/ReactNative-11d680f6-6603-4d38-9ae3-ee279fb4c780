19bbf298c21fc5f86ee9d7a5ba8abb44fe272da3
vap1 <vaibhavvasant1705@gmail.com>
2023-08-27 17:01:24 +0530

    First Commit

--- src/apis/SearchApi.ts
+++ src/apis/SearchApi.ts
@@ -9,6 +9,6 @@ export const search = async (keyword: string): Promise<SearchResponse> => {
     const response = await axios.get<SearchResponse>(`${BASE_URL}/search/${keyword}`);
     return response.data;
   } catch (error) {
-    throw new Error('Failed to perform search');
+    throw new Error('Failed to search. Please try again.'); // Handle error based on your app's requirements
   }
 };
